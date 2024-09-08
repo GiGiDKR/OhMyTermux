8ae3601c39cd1b6b3c284e48e5bd281d99ad25f6
GiGiDKR <74984020+GiGiDKR@users.noreply.github.com>
2024-09-09 00:28:33 +0200

    1.0.6

--- install.sh
+++ install.sh
@@ -66,9 +66,9 @@ if $ONLY_GUM; then
     SCRIPT_CHOICE=true
 fi
 
-#############
-# Fonctions #
-#############
+############
+# FUNCTION #
+############
 
 check_and_install_gum() {
     if $USE_GUM && ! command -v gum &> /dev/null; then
@@ -128,6 +128,10 @@ show_banner() {
 }
 
 show_banner
+
+##################
+# INITIAL CONFIG #
+##################
 
 initial_config() {
 echo -e "\e[38;5;33mChanger le répertoire de sources ? (o/n)\e[0m"
@@ -212,6 +216,10 @@ else
     [ "$choice" = "o" ] && termux-setup-storage
 fi
 }
+
+#########
+# SHELL #
+#########
 
 install_shell() {
     if $SHELL_CHOICE; then
@@ -337,6 +345,10 @@ install_shell() {
     fi
 }
 
+###############
+# PLUGINS ZSH #
+###############
+
 install_zsh_plugins() {
     if command -v zsh &> /dev/null; then
         show_banner
@@ -430,51 +442,59 @@ update_zshrc() {
     fi
 }
 
+############
+# PACKAGES #
+############
+
 install_packages() {
     if $PACKAGES_CHOICE; then
         show_banner
         if $USE_GUM; then
-            PACKAGES=$(gum choose --no-limit --selected.foreground="33" --header.foreground="33" --cursor.foreground="33" --height=19 --header="Sélectionner avec espace les packages à installer :" "nala" "eza" "bat" "lf" "fzf" "glow" "python" "nodejs" "nodejs-lts" "lsd" "micro" "vim" "neovim" "lazygit" "open-ssh" "tsu" "Tout installer")
+            PACKAGES=$(gum choose --no-limit --selected.foreground="33" --header.foreground="33" --cursor.foreground="33" --height=21 --header="Sélectionner avec espace les packages à installer :" "nala" "eza" "colorls" "lsd" "bat" "lf" "fzf" "glow" "tmux" "python" "nodejs" "nodejs-lts" "micro" "vim" "neovim" "lazygit" "open-ssh" "tsu" "Tout installer")
         else
             echo -e "\e[38;5;33mSélectionner les packages à installer (séparés par des espaces) :\e[0m"
             echo -e "\e[38;5;33m1) nala\e[0m"
             echo -e "\e[38;5;33m2) eza\e[0m"
-            echo -e "\e[38;5;33m3) bat\e[0m"
-            echo -e "\e[38;5;33m4) lf\e[0m"
-            echo -e "\e[38;5;33m5) fzf\e[0m"
-            echo -e "\e[38;5;33m6) glow\e[0m"
-            echo -e "\e[38;5;33m7) python\e[0m"
-            echo -e "\e[38;5;33m8) nodejs\e[0m"
-            echo -e "\e[38;5;33m9) nodejs-lts\e[0m"
-            echo -e "\e[38;5;33m10) lsd\e[0m"
-            echo -e "\e[38;5;33m11) micro\e[0m"
-            echo -e "\e[38;5;33m12) vim\e[0m"
-            echo -e "\e[38;5;33m13) neovim\e[0m"
-            echo -e "\e[38;5;33m14) lazygit\e[0m"
-            echo -e "\e[38;5;33m15) open-ssh\e[0m"
-            echo -e "\e[38;5;33m16) tsu\e[0m"
-            echo -e "\e[38;5;33m17) Tout installer\e[0m"
+            echo -e "\e[38;5;33m3) colorls\e[0m"   
+            echo -e "\e[38;5;33m4) lsd\e[0m"         
+            echo -e "\e[38;5;33m5) bat\e[0m"
+            echo -e "\e[38;5;33m6) lf\e[0m"
+            echo -e "\e[38;5;33m7) fzf\e[0m"
+            echo -e "\e[38;5;33m8) glow\e[0m"
+            echo -e "\e[38;5;33m9) tmux\e[0m"
+            echo -e "\e[38;5;33m10) python\e[0m"
+            echo -e "\e[38;5;33m11) nodejs\e[0m"
+            echo -e "\e[38;5;33m12) nodejs-lts\e[0m"
+            echo -e "\e[38;5;33m13) micro\e[0m"
+            echo -e "\e[38;5;33m14) vim\e[0m"
+            echo -e "\e[38;5;33m15) neovim\e[0m"
+            echo -e "\e[38;5;33m16) lazygit\e[0m"
+            echo -e "\e[38;5;33m17) open-ssh\e[0m"
+            echo -e "\e[38;5;33m18) tsu\e[0m"
+            echo -e "\e[38;5;33m19) Tout installer\e[0m"
             read -p "Entrez les numéros des packages : " package_choices
             PACKAGES=""
             for choice in $package_choices; do
                 case $choice in
                     1) PACKAGES+="nala " ;;
                     2) PACKAGES+="eza " ;;
-                    3) PACKAGES+="bat " ;;
-                    4) PACKAGES+="lf " ;;
-                    5) PACKAGES+="fzf " ;;
-                    6) PACKAGES+="glow " ;;
-                    7) PACKAGES+="python " ;;
-                    8) PACKAGES+="nodejs " ;;
-                    9) PACKAGES+="nodejs-lts " ;;
-                    10) PACKAGES+="lsd " ;;
-                    11) PACKAGES+="micro " ;;
-                    12) PACKAGES+="vim " ;;
-                    13) PACKAGES+="neovim " ;;
-                    14) PACKAGES+="lazygit " ;;
-                    15) PACKAGES+="open-ssh " ;;
-                    16) PACKAGES+="tsu " ;;
-                    17) PACKAGES="nala eza bat lf fzf glow python nodejs nodejs-lts lsd micro vim neovim lazygit open-ssh tsu" ;;
+                    3) PACKAGES+="colorsls " ;;
+                    4) PACKAGES+="lsd " ;;
+                    5) PACKAGES+="bat " ;;
+                    6) PACKAGES+="lf " ;;
+                    7) PACKAGES+="fzf " ;;
+                    8) PACKAGES+="glow " ;;
+                    9) PACKAGES+="tmux " ;;
+                    10) PACKAGES+="python " ;;
+                    11) PACKAGES+="nodejs " ;;
+                    12) PACKAGES+="nodejs-lts " ;;
+                    13) PACKAGES+="micro " ;;
+                    14) PACKAGES+="vim " ;;
+                    15) PACKAGES+="neovim " ;;
+                    16) PACKAGES+="lazygit " ;;
+                    17) PACKAGES+="open-ssh " ;;
+                    18) PACKAGES+="tsu " ;;
+                    19) PACKAGES="nala eza colorsls lsd bat lf fzf glow tmux python nodejs nodejs-lts micro vim neovim lazygit open-ssh tsu" ;;
                 esac
             done
         fi
@@ -629,6 +649,10 @@ fi
 
 #rm "$HOME/.termux/colors.zip" >/dev/null 2>&1
 
+########
+# FONT #
+########
+
 install_font() {
     if $FONT_CHOICE; then
         show_banner
@@ -706,71 +730,74 @@ install_font() {
     fi
 }
 
-##################
-# OhMyTermuxXFCE #
-##################
+#################
+# XFCE / DEBIAN #
+#################
 
 install_xfce() {
     if $XFCE_CHOICE; then
         show_banner
-        if $USE_GUM; then
-            if gum confirm --prompt.foreground="33" --selected.background="33" " Installer OhMyTermux XFCE ?"; then
-                username=$(gum input --placeholder "Entrez votre nom d'utilisateur")
-            else
-                PACKAGES="$PACKAGES ncurses-utils"
+        local install_xfce=false
 
-                for PACKAGE in $PACKAGES; do
-                    if $USE_GUM; then
-                        gum spin --spinner.foreground="33" --title.foreground="33" --title="Installation de $PACKAGE" -- pkg install -y $PACKAGE 
-                    fi
-                done
-                export PATH="$PATH:$PREFIX/bin"
-                show_banner
-                if gum confirm --prompt.foreground="33" --selected.background="33" " Exécuter OhMyTermux ?"; then
-                    termux-reload-settings
-                    rm -f install.sh
-                    clear
-                    exec $shell_choice
-                else
-                    termux-reload-settings
-                    rm -f install.sh
-                    echo -e "\e[38;5;33mOhMyTermux sera actif au prochain démarrage de Termux.\e[0m"
-                fi
-                exit 0
+        # Demander à l'utilisateur s'il veut installer XFCE et DEBIAN
+        if $USE_GUM; then
+            if gum confirm --prompt.foreground="33" --selected.background="33" " Installer XFCE et DEBIAN ?"; then
+                install_xfce=true
             fi
         else
-            echo -e "\e[38;5;33m Installer OhMyTermux XFCE ? (o/n)\e[0m"
+            echo -e "\e[38;5;33m Installer XFCE et DEBIAN ? (o/n)\e[0m"
             read choice
             if [ "$choice" = "o" ]; then
-                read -p "Entrez votre nom d'utilisateur : " username
-            else
-                PACKAGES="$PACKAGES ncurses-utils"
-
-                for PACKAGE in $PACKAGES; do
-                    echo -e "\e[38;5;33mInstallation de $PACKAGE...\e[0m"
-                    pkg install -y $PACKAGE >/dev/null 2>&1
-                done
-                export PATH="$PATH:$PREFIX/bin"
-                show_banner
-                echo -e "\e[38;5;33m Exécuter OhMyTermux ? (o/n)\e[0m"
-                read choice
-                if [ "$choice" = "o" ]; then
-                    termux-reload-settings
-                    rm -f install.sh
-                    clear
-                    exec $shell_choice
-                else
-                    termux-reload-settings
-                    rm -f install.sh
-                    echo -e "\e[38;5;33mOhMyTermux sera actif au prochain démarrage de Termux.\e[0m"
-                fi
-                exit 0
+                install_xfce=true
             fi
         fi
 
+        # Si l'utilisateur ne veut pas installer XFCE, installer seulement les packages nécessaires
+        if ! $install_xfce; then
+            PACKAGES="ncurses-utils"
+            for PACKAGE in $PACKAGES; do
+                if $USE_GUM; then
+                    gum spin --spinner.foreground="33" --title.foreground="33" --title="Installation de $PACKAGE" -- pkg install -y $PACKAGE 
+                else
+                    echo -e "\e[38;5;33mInstallation de $PACKAGE...\e[0m"
+                    pkg install -y $PACKAGE >/dev/null 2>&1
+                fi
+            done
+            export PATH="$PATH:$PREFIX/bin"
+            show_banner
+            
+            # Demander à l'utilisateur s'il veut exécuter OhMyTermux
+            local execute_ohmytermux=false
+            if $USE_GUM; then
+                if gum confirm --prompt.foreground="33" --selected.background="33" " Exécuter OhMyTermux ?"; then
+                    execute_ohmytermux=true
+                fi
+            else
+                echo -e "\e[38;5;33m Exécuter OhMyTermux ? (o/n)\e[0m"
+                read choice
+                if [ "$choice" = "o" ]; then
+                    execute_ohmytermux=true
+                fi
+            fi
+
+            if $execute_ohmytermux; then
+                termux-reload-settings
+                rm -f install.sh
+                clear
+                exec $shell_choice
+            else
+                termux-reload-settings
+                rm -f install.sh
+                echo -e "\e[38;5;33mOhMyTermux sera actif au prochain démarrage de Termux.\e[0m"
+            fi
+            return
+        fi
+
+        # Installation de XFCE et DEBIAN
         show_banner
         pkgs=('wget' 'ncurses-utils' 'dbus' 'proot-distro' 'x11-repo' 'tur-repo' 'pulseaudio')
 
+        # Installation des pré-requis
         show_banner
         if $USE_GUM; then
             gum spin --spinner.foreground="33" --title.foreground="33" --title="Installation des pré-requis" -- pkg install ncurses-ui-libs && pkg uninstall dbus -y
@@ -779,6 +806,7 @@ install_xfce() {
             pkg install ncurses-ui-libs && pkg uninstall dbus -y
         fi
 
+        # Mise à jour des paquets
         show_banner
         if $USE_GUM; then
             gum spin --spinner.foreground="33" --title.foreground="33" --title="Mise à jour des paquets" -- pkg update -y
@@ -787,6 +815,7 @@ install_xfce() {
             pkg update -y
         fi
 
+        # Installation des paquets nécessaires
         show_banner
         if $USE_GUM; then
             gum spin --spinner.foreground="33" --title.foreground="33" --title="Installation des paquets nécessaires" -- pkg install "${pkgs[@]}" -y
@@ -795,6 +824,7 @@ install_xfce() {
             pkg install "${pkgs[@]}" -y
         fi
 
+        # Téléchargement des scripts
         show_banner
         if $USE_GUM; then
             gum spin --spinner.foreground="33" --title.foreground="33" --title="Téléchargement des scripts" -- bash -c "
@@ -810,55 +840,78 @@ install_xfce() {
         fi
         chmod +x *.sh
 
+        # Exécution des scripts
         show_banner
         if $USE_GUM; then
-            ./xfce.sh --gum $username
-            ./proot.sh --gum $username
+            ./xfce.sh --gum
+            ./proot.sh --gum
         else
-            ./xfce.sh $username
-            ./proot.sh $username
+            ./xfce.sh
+            ./proot.sh
         fi
         ./utils.sh
 
-        # Get username function for alias to execute Debian 
-        echo -e '\nfunction get_username() {
-  user_dir="$PREFIX/var/lib/proot-distro/installed-rootfs/debian/home/"
-  username=$(basename "$user_dir"/*)
-  echo $username
+        # Ajout de la fonction get_username et de l'alias debian
+        add_get_username_function
+    fi
 }
 
-alias debian="proot-distro login debian --shared-tmp --user $(get_username)"' >> $BASHRC
-
-        if [ -f "$ZSHRC" ]; then
-            echo -e '\nfunction get_username() {
-  user_dir="$PREFIX/var/lib/proot-distro/installed-rootfs/debian/home/"
-  username=$(basename "$user_dir"/*)
-  echo $username
+add_get_username_function() {
+    local function_text='
+function get_username() {
+    user_dir="$PREFIX/var/lib/proot-distro/installed-rootfs/debian/home/"
+    username=$(basename "$user_dir"/*)
+    echo $username
+}
+alias debian="proot-distro login debian --shared-tmp --user $(get_username)"
+'
+    echo -e "$function_text" >> "$BASHRC"
+    
+    if [ -f "$ZSHRC" ]; then
+        echo -e "$function_text" >> "$ZSHRC"
+    fi
 }
 
-alias debian="proot-distro login debian --shared-tmp --user $(get_username)"' >> $ZSHRC
+##############
+# Termux-X11 #
+##############
+
+install_termux_x11() {
+    show_banner
+    local install_x11=false
+
+    if $USE_GUM; then
+        if gum confirm --prompt.foreground="33" --selected.background="33" " Installer Termux-X11 ?"; then
+            install_x11=true
         fi
-        # Termux-X11 
+    else
+        echo -e "\e[38;5;33m Installer Termux-X11 ? (o/n)\e[0m"
+        read -r choice
+        if [ "$choice" = "o" ]; then
+            install_x11=true
+        fi
+    fi
+
+    if $install_x11; then
         show_banner
+        local apk_url="https://github.com/termux/termux-x11/releases/download/nightly/app-arm64-v8a-debug.apk"
+        local apk_file="$HOME/storage/downloads/termux-x11.apk"
+
         if $USE_GUM; then
-            if gum confirm --prompt.foreground="33" --selected.background="33" " Installer Termux-X11 ?"; then
-                show_banner
-                gum spin --spinner.foreground="33" --title.foreground="33" --title="Téléchargement de Termux-X11 APK" -- wget https://github.com/termux/termux-x11/releases/download/nightly/app-arm64-v8a-debug.apk
-                mv app-arm64-v8a-debug.apk $HOME/storage/downloads/
-                termux-open $HOME/storage/downloads/app-arm64-v8a-debug.apk
-                rm $HOME/storage/downloads/app-arm64-v8a-debug.apk
-            fi
+            gum spin --spinner.foreground="33" --title.foreground="33" --title="Téléchargement de Termux-X11 APK" -- wget "$apk_url" -O "$apk_file"
         else
-            echo -e "\e[38;5;33m Installer Termux-X11 ? (o/n)\e[0m"
-            read choice
-            if [ "$choice" = "o" ]; then
-                show_banner
-                echo -e "\e[38;5;33mTéléchargement de Termux-X11 APK...\e[0m"
-                wget https://github.com/termux/termux-x11/releases/download/nightly/app-arm64-v8a-debug.apk
-                mv app-arm64-v8a-debug.apk $HOME/storage/downloads/
-                termux-open $HOME/storage/downloads/app-arm64-v8a-debug.apk
-                rm $HOME/storage/downloads/app-arm64-v8a-debug.apk
-            fi
+            echo -e "\e[38;5;33mTéléchargement de Termux-X11 APK...\e[0m"
+            wget "$apk_url" -O "$apk_file"
+        fi
+
+        if [ -f "$apk_file" ]; then
+            termux-open "$apk_file"
+            echo -e "\e[38;5;33mVeuillez installer l'APK manuellement.\e[0m"
+            echo -e "\e[38;5;33mUne fois l'installation terminée, appuyez sur Entrée pour continuer.\e[0m"
+            read -r
+            rm "$apk_file"
+        else
+            echo -e "\e[38;5;31mErreur : Le téléchargement de l'APK a échoué.\e[0m"
         fi
     fi
 }
@@ -975,6 +1028,7 @@ install_shell
 install_packages
 install_font
 install_xfce
+install_termux_x11
 install_script
 
 #################
